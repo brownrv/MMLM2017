77b1f4f6730dc14f26a9affe1ce2c7620b098088
Ron Brown <rbrown@supplyforce.com>
2017-03-12 21:47:31 -0400

    Initialize.

---
+++ MMLM2017.R
@@ -0,0 +1,26 @@
+
+
+library(tidyverse)
+
+CompactResults_PrelimThru2017Day87 <- read.csv("Data/CompactResults_PrelimThru2017Day87.csv")
+game_flow_details_2010_2014 <- read.csv("Data/game_flow_details_2010-2014.csv")
+massey_ordinals_2003_2016 <- read.csv("Data/massey_ordinals_2003-2016.csv")
+massey_ordinals_2017 <- read.csv("Data/MasseyOrdinals_2017_PrelimThruDay128_69systems.csv")
+playerdata_roster <- read.csv("Data/playerdata_roster.csv")
+playerdata_statistics <- read.csv("Data/playerdata_statistics.csv")
+RegularSeasonCompactResults <- read.csv("Data/RegularSeasonCompactResults.csv")
+RegularSeasonDetailedResults <- read.csv("Data/RegularSeasonDetailedResults.csv")
+sample_submission <- read.csv("Data/sample_submission.csv")
+Seasons <- read.csv("Data/Seasons.csv")
+SeedRoundSlots <- read.csv("Data/SeedRoundSlots.csv")
+TeamCoaches_PrelimThru2017Day87 <- read.csv("Data/TeamCoaches_PrelimThru2017Day87.csv")
+TeamConferences_Thru2017 <- read.csv("Data/TeamConferences_Thru2017.csv")
+Teams <- read.csv("Data/Teams.csv")
+TeamSpellings <- read.csv("Data/TeamSpellings.csv")
+TourneyCompactResults <- read.csv("Data/TourneyCompactResults.csv")
+TourneyDetailedResults <- read.csv("Data/TourneyDetailedResults.csv")
+TourneySeeds <- read.csv("Data/TourneySeeds.csv")
+TourneySlots <- read.csv("Data/TourneySlots.csv")
+RegularSeasonCompactResults_2017 <- read.csv("Data/2017_PrelimThruDay124_CompactResults.csv")
+RegularSeasonDetailedResults_2017 <- read.csv("Data/2017_PrelimThruDay124_DetailedResults.csv")
+
